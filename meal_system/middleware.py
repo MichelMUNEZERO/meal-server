26196ec4cb26a221d92c00ec305da39eae2c4cee
MichelMUNEZERO <michelmunezero25@gmail.com>
2026-09-25 23:18:05 +0200

    Update middleware

--- meal_system/middleware.py
+++ meal_system/middleware.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse
+import logging
+
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse
+
+
+logger = logging.getLogger(__name__)
 
 
 class CorsMiddleware:
-    allow_origin = '*'
     allow_headers = 'Content-Type, Authorization, X-Requested-With'
     allow_methods = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
 
@@ -12,13 +17,20 @@ class CorsMiddleware:
     def __call__(self, request):
         if request.method == 'OPTIONS':
             response = HttpResponse(status=204)
-            return self._apply_headers(response)
+            return self._apply_headers(response, request)
 
-        response = self.get_response(request)
-        return self._apply_headers(response)
+        try:
+            response = self.get_response(request)
+        except Exception:
+            logger.exception('Unhandled API exception for %s %s', request.method, request.path)
+            response = JsonResponse({'detail': 'Internal server error'}, status=500)
+        return self._apply_headers(response, request)
 
-    def _apply_headers(self, response):
-        response['Access-Control-Allow-Origin'] = self.allow_origin
+    def _apply_headers(self, response, request):
+        origin = request.headers.get('Origin', '').rstrip('/')
+        if origin in getattr(settings, 'CORS_ALLOWED_ORIGINS', set()):
+            response['Access-Control-Allow-Origin'] = origin
+            response['Vary'] = 'Origin'
         response['Access-Control-Allow-Headers'] = self.allow_headers
         response['Access-Control-Allow-Methods'] = self.allow_methods
         response['Access-Control-Max-Age'] = '86400'
